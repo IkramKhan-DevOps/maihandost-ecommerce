57a17b131a4637cd451d6739d2be81a1a6f810f0
saqib-devops <saqibahmad778866@gmail.com>
2022-10-23 20:53:02 +0500

    app:cleaned

--- src/website/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.views.generic import TemplateView
-
-app_name = 'website'
-urlpatterns = [
-    path('', TemplateView.as_view(template_name='website/home.html')),
-    path('shop/', TemplateView.as_view(template_name='website/shop.html')),
-]
